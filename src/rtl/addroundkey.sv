3bfbca8bedb78264ff91969a41639f3a29b32a15
Yuma Ueda <cyan@0x00a1e9.dev>
2020-11-27 01:57:23 +0900

    Add AddRoundKey()

---
+++ src/rtl/addroundkey.sv
@@ -0,0 +1,22 @@
+module ADDROUNDKEY #(
+    parameter WORD = 32,
+    parameter NB   = 4
+) (
+    input  wire               clk, rst, i_valid,
+    input  wire [WORD*NB-1:0] i_block, i_roundkey,
+    output reg                o_valid,
+    output reg  [WORD*NB-1:0] o_block
+);
+
+always @(posedge clk) begin
+    if (!rst) begin
+        o_valid <= '0;
+        o_block <= '0;
+    end
+    else begin
+        o_valid <= i_valid;
+        o_block <= i_block ^ i_roundkey;
+    end
+end
+
+endmodule
