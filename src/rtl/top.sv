58e9d809edcc66c26d9e0300e866037fd5ffb3c1
Yuma Ueda <cyan@0x00a1e9.dev>
2020-11-28 14:41:17 +0900

    Update top.sv
    - set default_nettype to none
    - define time unit and time precision

--- src/rtl/top.sv
+++ src/rtl/top.sv
@@ -1,19 +1,33 @@
-`define WORD 4
-`define NB   4   // number of data words
+`default_nettype none
+`timescale       1ns / 1ps
 
-`define NK4      // 128 bit key mode
-/*
-`define NK6      // 196 bit key mode
-`define NK8      // 256 bit key mode
- */
+`define          WORD 32
+`define          NB   4   // number of data words
 
-`ifdef  NK4
-`define NK   4
-`define NR   10
-`elsif  NK6
-`define NK   6
-`define NR   12
-`elsif  NK8
-`define NK   8
-`define NR   14
+`define          NK4      // 128 bit key mode
+/*-----------------------------------------------
+`define          NK6      // 196 bit key mode
+`define          NK8      // 256 bit key mode
+ -----------------------------------------------*/
+
+`ifdef           NK4
+`define          NK   4
+`define          NR   10
+`elsif           NK6
+`define          NK   6
+`define          NR   12
+`elsif           NK8
+`define          NK   8
+`define          NR   14
 `endif
+
+module top #(
+    parameter WORD = `WORD,   // word size in bits
+    parameter NB   = `NB,    // number of data words
+    parameter NK   = `NK,  // keylength / wordsize
+    parameter NR   = `NR   // number of rounds
+) (
+
+);
+
+endmodule
