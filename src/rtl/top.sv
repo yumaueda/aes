b5c0c0c71e56e323f2f305469136d5d5ea526b41
Yuma Ueda <cyan@0x00a1e9.dev>
2020-11-30 12:06:29 +0900

    cleanup

--- src/rtl/top.sv
+++ src/rtl/top.sv
@@ -1,15 +1,14 @@
 `default_nettype none
-`timeunit        1ns
-`timeprecision   1ps
+`timescale       1ns / 1ps
 
 `define          WORD 32
 `define          NB   4   // number of data words
 
 `define          NK4      // 128 bit key mode
-/*-----------------------------------------------
+/*-------------------------------------------
 `define          NK6      // 196 bit key mode
 `define          NK8      // 256 bit key mode
- -----------------------------------------------*/
+ -------------------------------------------*/
 
 `ifdef           NK4
 `define          NK   4
@@ -24,9 +23,9 @@
 
 module top #(
     parameter WORD = `WORD,   // word size in bits
-    parameter NB   = `NB,    // number of data words
-    parameter NK   = `NK,  // keylength / wordsize
-    parameter NR   = `NR   // number of rounds
+    parameter NB   = `NB,     // number of data words
+    parameter NK   = `NK,     // keylength / wordsize
+    parameter NR   = `NR      // number of rounds
 ) (
 
 );
