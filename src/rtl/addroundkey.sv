480692136b0b64d9e6db0fcd0b912e52d8aaded7
Yuma Ueda <cyan@0x00a1e9.dev>
2020-11-30 12:10:30 +0900

    cleanup

--- src/rtl/addroundkey.sv
+++ src/rtl/addroundkey.sv
@@ -8,15 +8,15 @@ module addroundkey #(
     output reg  [WORD*NB-1:0] o_block
 );
 
-always @(posedge clk) begin
-    if (!rst) begin
-        o_valid <= '0;
-        o_block <= '0;
+    always @(posedge clk) begin
+        if (!rst) begin
+            o_valid <= '0;
+            o_block <= '0;
+        end
+        else begin
+            o_valid <= i_valid;
+            o_block <= i_block ^ i_roundkey;
+        end
     end
-    else begin
-        o_valid <= i_valid;
-        o_block <= i_block ^ i_roundkey;
-    end
-end
 
 endmodule
