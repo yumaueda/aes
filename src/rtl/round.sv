cc06b33b4f002d48dcd445e01645e1dce37df624
Yuma Ueda <cyan@0x00a1e9.dev>
2020-12-06 05:48:36 +0900

    Update testbench for the round module

--- src/rtl/round.sv
+++ src/rtl/round.sv
@@ -1,15 +1,23 @@
 module round #(
-    parameter WORD = 32,
-    parameter NB   = 4
+    parameter WORD  = 32,
+    parameter NB    = 4,
+    parameter FINAL = 0
 ) (
-    input  wire               clk, rst, i_valid,
-    input  wire [WORD*NB-1:0] i_block, i_roundkey,
+    input  wire               clk, rst,
+    input  wire               i_valid,
+    input  wire [WORD*NB-1:0] i_block,
+    input  wire [WORD*NB-1:0] i_roundkey,
     output wire               o_valid,
     output wire [WORD*NB-1:0] o_block
 );
 
-    wire               sub_valid_shift, shift_valid_mix, mix_valid_add;
-    wire [WORD*NB-1:0] sub_block_shift, shift_block_mix, mix_block_add;
+    wire               sub_valid_shift, shift_valid_regormix, regormix_valid_add;
+    wire [WORD*NB-1:0] sub_block_shift, shift_block_regormix, regormix_block_add;
+
+    // Each register has the value n clocks ago
+    reg  [WORD-1:0]    wi_0_staged1, wi_0_staged2, wi_0_staged3;
+    reg  [WORD-1:0]    wi_1_staged1, wi_1_staged2;
+    reg  [WORD-1:0]    wi_2_staged1;
 
     subbytes #(
         .WORD(WORD),
@@ -26,28 +34,56 @@ module round #(
     ) shift (
         .i_valid(sub_valid_shift),
         .i_block(sub_block_shift),
-        .o_valid(shift_valid_mix),
-        .o_block(shift_block_mix)
+        .o_valid(shift_valid_regormix),
+        .o_block(shift_block_regormix)
     );
 
-    mixcolumns #(
-        .WORD(WORD),
-        .NB(NB)
-    ) mix (
-        .*,
-        .i_valid(shift_valid_mix),
-        .i_block(shift_block_mix),
-        .o_valid(mix_valid_add),
-        .o_block(mix_block_add)
-    );
+    generate if (FINAL == 0) begin
+        mixcolumns #(
+            .WORD(WORD),
+            .NB(NB)
+        ) mix (
+            .*,
+            .i_valid(shift_valid_regormix),
+            .i_block(shift_block_regormix),
+            .o_valid(regormix_valid_add),
+            .o_block(regormix_block_add)
+        );
+    end
+    else begin
+        // FINAL == 1
+        // Wait for two cycles until the key expansion is complete
+        reg               shift_valid_staged1, shift_valid_staged2;
+        reg [WORD*NB-1:0] shift_block_staged1, shift_block_staged2;
+
+        always @(posedge clk) begin
+            shift_valid_staged1 <= shift_valid_regormix;
+            shift_valid_staged2 <= shift_valid_staged1;
+            shift_block_staged1 <= shift_block_regormix;
+            shift_block_staged2 <= shift_block_staged1;
+        end
+    end
+    endgenerate
 
     addroundkey #(
         .WORD(WORD),
         .NB(NB)
     ) add (
         .*,
-        .i_valid(mix_valid_add),
-        .i_block(mix_block_add)
+        .i_valid(regormix_valid_add),
+        .i_block(regormix_block_add),
+        .i_roundkey({wi_0_staged3, wi_1_staged2, wi_2_staged1, i_roundkey[31:0]})
     );
+
+    always @(posedge clk) begin
+        wi_0_staged1 <= i_roundkey[127:96];
+        wi_0_staged2 <= wi_0_staged1;
+        wi_0_staged3 <= wi_0_staged2;
+
+        wi_1_staged1 <= i_roundkey[95:64];
+        wi_1_staged2 <= wi_1_staged1;
+
+        wi_2_staged1 <= i_roundkey[63:32];
+    end
 
 endmodule
---
+++ src/tb/sim_round.cpp
@@ -0,0 +1,126 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vround.h"
+
+#define TRACE_LEVEL                 99
+#define MASTER_CLK_HALF             (10/2)          // 100MHz
+
+// Cipher
+#define FIPS_CIPHER_START3          0x193de3be
+#define FIPS_CIPHER_START2          0xa0f4e22b
+#define FIPS_CIPHER_START1          0x9ac68d2a
+#define FIPS_CIPHER_START0          0xe9f84808
+
+#define FIPS_CIPHER_ROUND_KEY3      0xa0fafe17
+#define FIPS_CIPHER_ROUND_KEY2      0x88542cb1
+#define FIPS_CIPHER_ROUND_KEY1      0x23a33939
+#define FIPS_CIPHER_ROUND_KEY0      0x2a6c7605
+/*--------------------------------------------
+#define FIPS_CIPHER_AFT_SUB3        0xd42711ae
+#define FIPS_CIPHER_AFT_SUB2        0xe0bf98f1
+#define FIPS_CIPHER_AFT_SUB1        0xb8b45de5
+#define FIPS_CIPHER_AFT_SUB0        0x1e415230
+
+#define FIPS_CIPHER_AFT_SHIFT3      0xd4bf5d30
+#define FIPS_CIPHER_AFT_SHIFT2      0xe0b452ae
+#define FIPS_CIPHER_AFT_SHIFT1      0xb84111f1
+#define FIPS_CIPHER_AFT_SHIFT0      0x1e2798e5
+
+#define FIPS_CIPHER_AFT_MIX3        0x046681e5
+#define FIPS_CIPHER_AFT_MIX2        0xe0cb199a
+#define FIPS_CIPHER_AFT_MIX1        0x48f8d37a
+#define FIPS_CIPHER_AFT_MIX0        0x2806264c
+ --------------------------------------------*/
+
+#define FIPS_CIPHER_AFT_ADD3        0xa49c7ff2
+#define FIPS_CIPHER_AFT_ADD2        0x689f352b
+#define FIPS_CIPHER_AFT_ADD1        0x6b5bea43
+#define FIPS_CIPHER_AFT_ADD0        0x026a5049
+
+vluint64_t main_time = 0;
+
+double sc_time_stamp(void) {
+    return main_time;
+}
+
+int main(int argc, char **argv)
+{
+    // TODO: Use FST instead of VCD
+    Verilated::commandArgs(argc, argv);
+    Verilated::traceEverOn(true);
+
+    // Round
+    Vround *top = new Vround;
+    VerilatedVcdC *tfp = new VerilatedVcdC;
+    bool pass = false;
+
+    top->trace(tfp, TRACE_LEVEL);
+    tfp->open("sim_round.vcd");
+
+    printf("--- Verilator testbench for the round module ---\n");
+    printf("Input State:\t{%#08x, %#08x, %#08x, %#08x}\n",
+            FIPS_CIPHER_START3,
+            FIPS_CIPHER_START2,
+            FIPS_CIPHER_START1,
+            FIPS_CIPHER_START0);
+    printf("Round Key:\t{%#08x, %#08x, %#08x, %#08x}\n",
+            FIPS_CIPHER_ROUND_KEY3,
+            FIPS_CIPHER_ROUND_KEY2,
+            FIPS_CIPHER_ROUND_KEY1,
+            FIPS_CIPHER_ROUND_KEY0);
+
+    top->clk = 1;
+    top->rst = 0;
+    top->i_valid = 1;
+    top->i_block[0] = FIPS_CIPHER_START0;
+    top->i_block[1] = FIPS_CIPHER_START1;
+    top->i_block[2] = FIPS_CIPHER_START2;
+    top->i_block[3] = FIPS_CIPHER_START3;
+    top->i_roundkey[0] = FIPS_CIPHER_ROUND_KEY0;
+    top->i_roundkey[1] = FIPS_CIPHER_ROUND_KEY1;
+    top->i_roundkey[2] = FIPS_CIPHER_ROUND_KEY2;
+    top->i_roundkey[3] = FIPS_CIPHER_ROUND_KEY3;
+
+    while (sc_time_stamp() < 50) {
+        if (main_time % MASTER_CLK_HALF == 0) {
+            top->clk = top->clk ? 0 : 1;
+            if (top->clk && !top->rst)
+                top->rst = 1;
+            else if (top->clk && top->i_valid)
+                top->i_valid = 0;
+        }
+        top->eval();
+        tfp->dump(++main_time);
+    }
+
+    printf("Output State:\t{%#08x, %#08x, %#08x, %#08x}\n",
+            top->o_block[3],
+            top->o_block[2],
+            top->o_block[1],
+            top->o_block[0]);
+
+    printf("Expected State:\t{%#08x, %#08x, %#08x, %#08x}\n",
+            top->o_block[3],
+            top->o_block[2],
+            top->o_block[1],
+            top->o_block[0]);
+
+    pass = top->o_block[0] == FIPS_CIPHER_AFT_ADD0 &&
+           top->o_block[1] == FIPS_CIPHER_AFT_ADD1 &&
+           top->o_block[2] == FIPS_CIPHER_AFT_ADD2 &&
+           top->o_block[3] == FIPS_CIPHER_AFT_ADD3;
+
+    printf("Result: \x1b[1m");
+    if (pass)
+        printf("\x1b[32m" "\tPass\n");
+    else
+        printf("\x1b[31m" "\tFail\n");
+
+    tfp->close();
+    top->final();
+    delete top;
+
+    return 0;
+}
