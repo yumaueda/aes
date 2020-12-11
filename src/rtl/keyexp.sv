377f9107061a2f24e26fd93c9bf54537cc01417f
Yuma Ueda <cyan@0x00a1e9.dev>
2020-12-12 00:01:43 +0900

    Output first NK words via a latch

--- src/rtl/keyexp.sv
+++ src/rtl/keyexp.sv
@@ -10,6 +10,7 @@ module keyexp #(
     output wire [WORD-1:0]    o_w [NB*(NR+1)-1:0]
 );
 
+    reg  [WORD*NK-1:0]    key;
     wire [NB*(NR+1)-NK:0] keu_valid;
     wire [WORD-1:0]       keu_wi_1  [NB*(NR+1)-NK:0];
     wire [WORD-1:0]       keu_wi_nk [NB*(NR+1)-NK-1:0];
@@ -28,9 +29,16 @@ module keyexp #(
         reg [WORD-1:0] key_w0_staged3;
 
         always @(posedge clk) begin
-            key_w2_staged1 <= i_key[95:64];
-            key_w1_staged1 <= i_key[63:32];
-            key_w0_staged1 <= i_key[31:0];
+            if (!rst) begin
+                key_w2_staged1 <= '0;
+                key_w1_staged1 <= '0;
+                key_w0_staged1 <= '0;
+            end
+            else if (i_valid) begin
+                key_w2_staged1 <= i_key[95:64];
+                key_w1_staged1 <= i_key[63:32];
+                key_w0_staged1 <= i_key[31:0];
+            end
 
             key_w1_staged2 <= key_w1_staged1;
             key_w0_staged2 <= key_w0_staged1;
@@ -64,11 +72,20 @@ module keyexp #(
         reg [WORD-1:0] key_w0_staged5;
 
         always @(posedge clk) begin
-            key_w4_staged1 <= i_key[159:128];
-            key_w3_staged1 <= i_key[127:96];
-            key_w2_staged1 <= i_key[95:64];
-            key_w1_staged1 <= i_key[63:32];
-            key_w0_staged1 <= i_key[31:0];
+            if (!rst) begin
+                key_w4_staged1 <= '0;
+                key_w3_staged1 <= '0;
+                key_w2_staged1 <= '0;
+                key_w1_staged1 <= '0;
+                key_w0_staged1 <= '0;
+            end
+            else if (i_valid) begin
+                key_w4_staged1 <= i_key[159:128];
+                key_w3_staged1 <= i_key[127:96];
+                key_w2_staged1 <= i_key[95:64];
+                key_w1_staged1 <= i_key[63:32];
+                key_w0_staged1 <= i_key[31:0];
+            end
 
             key_w3_staged2 <= key_w3_staged1;
             key_w2_staged2 <= key_w2_staged1;
@@ -128,14 +145,24 @@ module keyexp #(
         reg [WORD-1:0] key_w0_staged7;
 
         always @(posedge clk) begin
-            key_w6_staged1 <= i_key[223:192];
-            key_w5_staged1 <= i_key[191:160];
-            key_w4_staged1 <= i_key[159:128];
-            key_w3_staged1 <= i_key[127:96];
-            key_w2_staged1 <= i_key[95:64];
-            key_w1_staged1 <= i_key[63:32];
-            key_w0_staged1 <= i_key[31:0];
-
+            if (!rst) begin
+                key_w6_staged1 <= '0;
+                key_w5_staged1 <= '0;
+                key_w4_staged1 <= '0;
+                key_w3_staged1 <= '0;
+                key_w2_staged1 <= '0;
+                key_w1_staged1 <= '0;
+                key_w0_staged1 <= '0;
+            end
+            else if (i_valid) begin
+                key_w6_staged1 <= i_key[223:192];
+                key_w5_staged1 <= i_key[191:160];
+                key_w4_staged1 <= i_key[159:128];
+                key_w3_staged1 <= i_key[127:96];
+                key_w2_staged1 <= i_key[95:64];
+                key_w1_staged1 <= i_key[63:32];
+                key_w0_staged1 <= i_key[31:0];
+            end
             key_w5_staged2 <= key_w5_staged1;
             key_w4_staged2 <= key_w4_staged1;
             key_w3_staged2 <= key_w3_staged1;
@@ -174,25 +201,37 @@ module keyexp #(
     end
     endgenerate
 
+    always_latch begin
+        if (!rst) begin
+            key = '0;
+        end
+        else if (i_valid) begin
+            key = i_key;
+        end
+    end
+
     generate
         genvar i;
 
-        for (i = 0; i < NB*(NR+1); i += 1) begin
-            assign o_w[i] = i < NK ?
-                i_key[WORD*(NK-i)-1:WORD*(NK-i-1)] : keu_wi_1[i-NK+1];
-        end
-
         for (i = 0; i < NK; i += 1) begin
+            assign o_w[i] = key[WORD*(NK-i)-1:WORD*(NK-i-1)];
         end
 
         for (i = NK; i < NB*(NR+1)-NK; i += 1) begin
-            assign keu_wi_nk[i] = w_staged3[i-NK];
-
             always @(posedge clk) begin
-                w_staged1[i-NK] <= o_w[i];
-                w_staged2[i-NK] <= w_staged1[i-NK];
-                w_staged3[i-NK] <= w_staged2[i-NK];
+                if (!rst) begin
+                    w_staged1[i-NK] <= '0;
+                    w_staged2[i-NK] <= '0;
+                    w_staged3[i-NK] <= '0;
+                end
+                else begin
+                    w_staged1[i-NK] <= o_w[i];
+                    w_staged2[i-NK] <= w_staged1[i-NK];
+                    w_staged3[i-NK] <= w_staged2[i-NK];
+                end
             end
+
+            assign keu_wi_nk[i] = w_staged3[i-NK];
         end
 
         for (i = NK; i < NB*(NR+1); i += 1) begin
@@ -210,6 +249,8 @@ module keyexp #(
                 .o_valid(keu_valid[i-NK+1]),
                 .o_wi(keu_wi_1[i-NK+1])
             );
+
+            assign o_w[i] = keu_wi_1[i-NK+1];
         end
     endgenerate
 
