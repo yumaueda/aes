b11fc0c9011306f4e481da2bd29a37f484a4760d
Yuma Ueda <cyan@0x00a1e9.dev>
2020-12-11 17:37:37 +0900

    Create the testbench for the round module

--- src/tb/tb_round.sv
+++ src/tb/tb_round.sv
@@ -2,43 +2,57 @@
 `timescale       1ns / 1ps
 `include         "param.svh"
 
-interface round_if # (
-    parameter WORD = 32,
-    parameter NB   = 4,
-) (
-    input logic clk, rst
-)
+program round_test (
+);
+endprogram
 
+module tb_round (
+    input  i_valid,
+    input  i_block,
+    input  i_roundkey,
+    input  o_valid,
+    output o_block
+);
+
+    localparam WORD  = `WORD;
+    localparam NB    = `NB;
+    localparam FINAL = 0;
+
+    logic               clk, rst;
     logic               i_valid;
     logic [WORD*NB-1:0] i_block;
     logic [WORD*NB-1:0] i_roundkey;
     logic               o_valid;
     logic [WORD*NB-1:0] o_block;
 
-endinterface
-
-`ifndef VERILATOR
-program round_test (
-);
-endprogram
-`endif
-
-module tb_round;
-
-    localparam CYCLE = 10;
-
-    localparam WORD  = `WORD,
-    localparam NB    = `NB,
-    localparam FINAL = 0
+    default clocking master @(posedge clk);
+        default input #1 output #0;
+        input  rst;
+        input  i_valid;
+        input  i_block;
+        input  i_roundkey;
+        output o_valid;
+        output o_block;
+    endclocking : master
 
     round #(
         .WORD(WORD),
         .NB(NB),
         .FINAL(FINAL)
     ) dut (
+        .*
     );
 
     initial begin
+        ##5 $finish();
+    end
+
+    always #(10/2) clk = ~clk;
+
+    initial begin
+        rst = '0;
+        #4;
+        rst = '1;
     end
 
 endmodule
